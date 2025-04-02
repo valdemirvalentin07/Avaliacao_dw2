66796d106422f59df49eb2a331a7b6e8d2a914be
Gahlego valentin <valdemirvalentim762@gmail.com>
2025-04-02 19:28:00 -0300

    Update cadastrarlivros.php

--- cadastrarlivros.php
+++ cadastrarlivros.php
@@ -1,26 +1,30 @@
 <?php
  
 session_start(); // Inicia a sessão
- 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-       
-        if(!isset($_SESSION['loggedin'])){
-            header("location: biblio.php");
-            exit;
-        }else{
-            $titulo_do_livro = $_POST['titulodolivro'];
-            $autor = $_POST['autor'];
-            $editora = $_POST['editora'];
-            $isbn = $_POST['isbn'];
- 
- 
-            $info = " $titulo_do_livro | $autor | $editora | $isbn | \n\n";
-    $handle = fopen("livros.txt", "a");
-    fwrite($handle, $info);
- 
-   }
+
+// Verifica se o formulário foi enviado via método POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Verifica se o usuário está logado
+    if (!isset($_SESSION['loggedin'])) {
+        // Redireciona para a página "biblio.php" caso o usuário não esteja logado
+        header("location: biblio.php");
+        exit;
+    } else {
+        // Captura os dados enviados pelo formulário
+        $titulo_do_livro = $_POST['titulodolivro'];
+        $autor = $_POST['autor'];
+        $editora = $_POST['editora'];
+        $isbn = $_POST['isbn'];
+
+        // Formata as informações do livro para serem salvas no arquivo "livros.txt"
+        $info = " $titulo_do_livro | $autor | $editora | $isbn | \n\n";
+        
+        // Abre o arquivo "livros.txt" no modo de escrita (append) e grava os dados
+        $handle = fopen("livros.txt", "a");
+        fwrite($handle, $info);
+    }
 }
-    
 ?>
 
 <!DOCTYPE html>
@@ -34,24 +38,27 @@ session_start(); // Inicia a sessão
 
 <h2>Cadastros para livros</h2>
 <br>
+
+<!-- Formulário de cadastro de livros -->
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
         <label for="titulo do livro">Título do livro</label>
         <input type="text" name="titulodolivro" id="titulodolivro">
         <br><br/>
+        
         <label for="autor">Autor</label>
         <input type="text" name="autor" id="autor">
         <br><br/>
+        
         <label for="editora">Editora</label>
         <input type="text" name="editora" id="editora">
         <br><br/>
+        
         <label for="isbn">ISBN</label>
         <input type="text" name="isbn" id="isbn">
         <br><br/>
 
-        <input type="submit"  value="enviar">
-    </form>
-    
-   
-    
+        <input type="submit" value="enviar">
+</form>
+
 </body>
 </html>
